ee0b8bac58e8c412ec3d9db1ec657f1cd2cce7cd
Alexandre Demiate <alexandre.demiate@gmail.com>
2017-10-21 14:22:39 -0200

    Implementação imshow

--- Teste.py
+++ Teste.py
@@ -1,1 +1,16 @@
 # Teste de processamento
+
+import cv2
+import numpy as np
+
+cap =cv2.VideoCapture(0)
+while (cap.isOpened()):
+    ret,img =cap.read()
+    cv2.imshow('output',img)
+
+    k=cv2.waitKey(10)
+    if k== 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
