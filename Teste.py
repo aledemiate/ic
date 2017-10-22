fc4c0338ffe1e9cce7e37dbf7f86547ec5f201ed
Alexandre Demiate <alexandre.demiate@gmail.com>
2017-10-22 14:08:13 -0200

    Implementacao do filtro binary thresholded

--- Teste.py
+++ Teste.py
@@ -1,15 +1,53 @@
 # Teste de processamento
 
+# Importacao das Bibliotecas:
 import cv2
 import numpy as np
 
-cap =cv2.VideoCapture(0)
-while (cap.isOpened()):
-    ret,img =cap.read()
+# Definicao da Camera usada:
+# 0 - Nao utilizado
+# 1 - Camera onboard do Notebook
+cap = cv2.VideoCapture(1)
+# 2 - Camera externa (USB)
+#cap = cv2.VideoCapture(2)
+
+while (cap.isOpened()) :
+    
+    # Captura da imagem pela Camera
+    ret,img = cap.read()
     cv2.imshow('output',img)
 
-    k=cv2.waitKey(10)
-    if k== 27:
+    # Aplicacao do filtro 'escala de cinza'
+    #img2 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    #cv2.imshow('grayscale', img2)
+
+    #-----------------------------------------------
+    #-- UTILIZACAO DO FILTRO 'BINARY THRESHOLDED' --
+    #-----------------------------------------------
+
+    # Converter de RGB para HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # Definir o range de variacao das cores em HSV
+
+    # - AZUL
+    #minimo = np.array([110,50,50])
+    #maximo = np.array([130,255,255])
+
+    # - VERMELHO - (valores pequenos)
+    minimo = np.array([0,150,150])
+    maximo = np.array([10,255,255])
+
+    # - VERMELHO - (valores grandes)
+    #minimo = np.array([170,70,50])
+    #maximo = np.array([180,255,255])
+
+    # Aplicacao do filtro 'binary thresholded'
+    imgthreshold = cv2.inRange(hsv, minimo, maximo)
+    cv2.imshow('Thresholded', imgthreshold)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
         break
 
 cap.release()
